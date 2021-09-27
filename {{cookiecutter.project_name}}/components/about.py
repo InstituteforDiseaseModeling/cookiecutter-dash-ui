69780a66efdff9245e00bf398542315f5eb1f790
Bee Workeneh <84352386+bworkeneh-IDM@users.noreply.github.com>
2021-09-27 15:34:50 -0700

    add style.css

--- {{cookiecutter.project_name}}/components/about.py
+++ {{cookiecutter.project_name}}/components/about.py
@@ -1,23 +1,7 @@
 from dash import html
 
-about_style = {
-    "minWidth": '48rem',
-    "width": '70%',
-    "margin": '0 auto',
-}
-title_style = {
-    "margin": '20px 20px 10px 0px',
-    "color": "#F1815E",
-}
-content_text_style = {
-    "fontFamily": '"Roboto", "Helvetica", "Arial", "sans-serif"',
-    "fontSize": 16,
-    "textAlign": 'justify',
-    "textJustify": 'inter-word'
-}
-
-about = html.Div(style=about_style, children=[
-    html.H4(style=title_style, children="What is {{cookiecutter.project_name}}?"),
-    html.P(style=content_text_style, children="{{cookiecutter.project_name}} is ..."),
-    html.P(style=content_text_style, children="Add titles and paragraphs as necessary")
+about = html.Div(className="content", children=[
+    html.H4( className="title titleText" , children="What is {{cookiecutter.project_name}}?"),
+    html.P(children="{{cookiecutter.project_name}} is ..."),
+    html.P(children="Add titles and paragraphs as necessary")
 ])
